618b355233b747ad924a037984bc863b6ba80398
DimiLyb <mrwgino@gmail.com>
2016-04-15 20:50:56 +0200

    setting mainpage

--- gittyhub/gittyhub/urls.py
+++ gittyhub/gittyhub/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'mainapp.views.index'),
     url(r'^mainapp/', include('mainapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
