aabca2bf5baada1b95d85059ab279d9e2d43f7d8
DimiLyb <mrwgino@gmail.com>
2016-04-18 17:36:00 +0200

    download os fix

--- gittyhub/mainapp/repo.py
+++ gittyhub/mainapp/repo.py
@@ -1,5 +1,5 @@
 from django import forms
-import urllib.request, json, shutil, os, zipfile, base64
+import urllib.request, json, shutil, os, zipfile, base64, platform
 
 def getrepo(valu):
     with urllib.request.urlopen(valu) as url: s = url.read()
@@ -13,10 +13,22 @@ def getjson(valu):
     return data
 
 def getfile(url, file_name):
-    #platform.system()
-    filename = os.path.join('C:/ap/repos', file_name)
+    plat = platform.system()
+    filename = ""
+    if plat == "windows":
+        filename = os.path.join('C:/ap/repos', file_name)
+    else:
+        filename = os.path.join('/ap/repos', file_name)
+        
     with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:shutil.copyfileobj(response, out_file)
     getunzip(file_name)
     
 def getunzip(file_name):
-    with zipfile.ZipFile(r'C:\ap\repos\\' + file_name, "r") as z: z.extractall(r'C:\ap\repos\\')
+    plat = platform.system()
+    filename = ""
+    if plat == "windows":
+        with zipfile.ZipFile(r'C:\ap\repos\\' + file_name, "r") as z: z.extractall(r'C:\ap\repos\\')
+    else:
+        with zipfile.ZipFile(r'/ap/repos/' + file_name, "r") as z: z.extractall(r'/ap/repos/')
+        
+    #with zipfile.ZipFile(r'C:\ap\repos\\' + file_name, "r") as z: z.extractall(r'C:\ap\repos\\')
