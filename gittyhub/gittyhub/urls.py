ddd825d493052bb700785d51b6ef3134f3f70093
DimiLyb <mrwgino@gmail.com>
2016-04-18 19:29:10 +0200

    update url

--- gittyhub/gittyhub/urls.py
+++ gittyhub/gittyhub/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from mainapp import views as mainapp_views
 
 urlpatterns = [
-    url(r'^$', 'mainapp.views.index'),
+    url(r'^$', mainapp_views.index),
     url(r'^mainapp/', include('mainapp.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^thanks/', 'mainapp.views.thanks'),
-    url(r'^download/(?P<owner>[\w|\W]+)/(?P<repo>[\w|\W]+)/(?P<fork>[\w|\W]+)/$', 'mainapp.views.download'),
-    url(r'^commit/(?P<owner>[\w|\W]+)/(?P<repo>[\w|\W]+)/$', 'mainapp.views.commit'),
+    url(r'^download/(?P<owner>[\w|\W]+)/(?P<repo>[\w|\W]+)/(?P<fork>[\w|\W]+)/$', mainapp_views.download),
+    url(r'^commit/(?P<owner>[\w|\W]+)/(?P<repo>[\w|\W]+)/$', mainapp_views.commit),
     #url(r'^partner/(?P<author>[-\w]+)/(?P<video>\w+)/(?P<related>\w+)/$', 'video_player'),
 ]
 
